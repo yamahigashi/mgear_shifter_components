86b64d777b1e500ff6c1f620033354089f4971c9
yamahigashi <yamahigashi@gmail.com>
2021-09-12 00:02:36 +0900

    fix missing function

--- python/ymt_shifter_utility/__init__.py
+++ python/ymt_shifter_utility/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import sys
+
 from Qt import QtWidgets
+
+
+import mgear.shifter.component as component
 import mgear.synoptic as synoptic
 
 from mgear.core import (
@@ -15,6 +20,29 @@ from mgear.core import (
     primitive,
     utils,
 )
+
+from logging import (  # noqa:F401 pylint: disable=unused-import, wrong-import-order
+    StreamHandler,
+    getLogger,
+    WARN,
+    DEBUG,
+    INFO
+)
+
+if sys.version_info >= (3, 0):  # pylint: disable=using-constant-test  # pylint: disable=using-constant-test, wrong-import-order
+    # For type annotation
+    from typing import (  # NOQA: F401 pylint: disable=unused-import
+        Optional,
+        Dict,
+        List,
+        Tuple,
+        Pattern,
+        Callable,
+        Any,
+        Text,
+        Generator,
+        Union
+    )
 
 
 def collect_synoptic_windows(parent=None):
@@ -56,10 +84,12 @@ def hide():
         w.showMinimized()
 
 
-def setKeyableAttributesDontLockVisibility(nodes,
-                                           params=["tx", "ty", "tz",
-                                                   "ro", "rx", "ry", "rz",
-                                                   "sx", "sy", "sz"]):
+def setKeyableAttributesDontLockVisibility(nodes, params=None):
+
+    if not params:
+        params = ["tx", "ty", "tz",
+                  "ro", "rx", "ry", "rz",
+                  "sx", "sy", "sz"]
 
     attribute.setKeyableAttributes(nodes, params)
 
@@ -126,3 +156,44 @@ def _findPathAtoB(aPath, bPath):
     up = list(reversed(bPath[:(idx)]))
 
     return down, sharedNode, up
+
+
+def addCtlMetadata(self, ctl):
+    # type: (component.Main, pm.datatypes.Transform) -> None
+
+    name = ctl.name()
+
+    attribute.addAttribute(ctl, "isCtl", "bool", keyable=False)
+    attribute.addAttribute(ctl, "uiHost", "string", keyable=False)
+    ctl.addAttr("uiHost_cnx", at='message', multi=False)
+    # set the control Role for complex components. If the component is
+    # of type control_01 or world_ctl the control role will default to None
+    # since is only one control the role is not needed
+    attribute.addAttribute(
+        ctl, "ctl_role", "string", keyable=False, value=name)
+
+    # mgear name. This keep track of the default shifter name. This naming
+    # system ensure that each control has a unique id. Tools like mirror or
+    # flip pose can use it to track symmetrical controls
+    attribute.addAttribute(ctl,
+                           "shifter_name",
+                           "string",
+                           keyable=False,
+                           value=self.getName(name) + "_ctl")
+    attribute.addAttribute(
+        ctl, "side_label", "string", keyable=False, value=self.side)
+    attribute.addAttribute(ctl,
+                           "L_custom_side_label",
+                           "string",
+                           keyable=False,
+                           value=self.options["side_left_name"])
+    attribute.addAttribute(ctl,
+                           "R_custom_side_label",
+                           "string",
+                           keyable=False,
+                           value=self.options["side_right_name"])
+    attribute.addAttribute(ctl,
+                           "C_custom_side_label",
+                           "string",
+                           keyable=False,
+                           value=self.options["side_center_name"])
