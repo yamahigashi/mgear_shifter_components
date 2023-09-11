852eed4eec1ac9e047178d92db9be6b6aa205719
yamahigashi <yamahigashi@gmail.com>
2023-09-11 12:58:58 +0900

    add: new component ghost/rivet controller

---
+++ python/ymt_components/ymt_surface_ghost_01/__init__.py
@@ -0,0 +1,368 @@
+"""mGear shifter components"""
+# pylint: disable=import-error,W0201,C0111,C0112
+import re
+import math
+import functools
+
+import maya.cmds as cmds
+import maya.OpenMaya as om1
+import maya.api.OpenMaya as om
+
+import pymel.core as pm
+from pymel.core import datatypes
+
+import exprespy.cmd
+from mgear.shifter import component
+
+from mgear.core import (
+    transform,
+    # curve,
+    applyop,
+    attribute,
+    icon,
+    fcurve,
+    vector,
+    meshNavigation,
+    node,
+    primitive,
+    utils,
+)
+from mgear.rigbits import ghost
+
+from mgear.core.transform import (
+    getTransform,
+    resetTransform,
+    # getTransformLookingAt,
+    # getChainTransform2,
+    setMatrixPosition,
+)
+
+from mgear.core.primitive import (
+    addTransform,
+)
+
+import ymt_shifter_utility as ymt_util
+import ymt_shifter_utility.curve as curve
+
+if False:  # pylint: disable=using-constant-test, wrong-import-order
+    # For type annotation
+    from typing import (  # NOQA: F401 pylint: disable=unused-import
+        Optional,
+        Dict,
+        List,
+        Tuple,
+        Pattern,
+        Callable,
+        Any,
+        Text,
+        Generator,
+        Union
+    )
+    from pathlib import Path  # NOQA: F401, F811 pylint: disable=unused-import,reimported
+    from types import ModuleType  # NOQA: F401 pylint: disable=unused-import
+    from six.moves import reload_module as reload  # NOQA: F401 pylint: disable=unused-import
+
+from logging import (  # noqa:F401 pylint: disable=unused-import, wrong-import-order
+    StreamHandler,
+    getLogger,
+    WARN,
+    DEBUG,
+    INFO
+)
+
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+logger.addHandler(handler)
+logger.propagate = False
+
+
+##########################################################
+# COMPONENT
+##########################################################
+class Component(component.Main):
+    """Shifter component Class"""
+
+    # =====================================================
+    # OBJECTS
+    # =====================================================
+    def addObjects(self):
+        """Add all the objects needed to create the component."""
+
+        self.WIP = self.options["mode"]
+
+        if self.negate and self.settings["overrideNegate"]:
+            self.negate = False
+            self.n_factor = 1
+
+        if self.settings["overrideNegate"]:
+            self.mirror_conf = [0, 0, 1,
+                                1, 1, 0,
+                                0, 0, 0]
+        else:
+            self.mirror_conf = [0, 0, 0,
+                                0, 0, 0,
+                                0, 0, 0]
+
+        # -------------------------------------------------------
+        self.ctlName = "ctl"
+        self.detailControllersGroupName = "controllers_detail"  # TODO: extract to settings
+        self.primaryControllersGroupName = "controllers_primary"  # TODO: extract to settings
+        self.connect_surface_slider = self.settings["isSlidingSurface"]
+        self.surfaceKeyable = self.settings["surfaceKeyable"]
+        self.sourceKeyable = self.settings["sourceKeyable"]
+        # --------------------------------------------------------
+        self.ik_ctl = []
+        self.ik_npo = []
+
+        # --------------------------------------------------------
+        self.previusTag = self.parentCtlTag
+        t = getTransform(self.root)
+        if self.negate:
+            scl = [-1, 1, 1]
+            t = transform.setMatrixScale(t, scl)
+
+        self.npo = addTransform(self.root, self.getName("npo"), t)
+        self.surfaceCtl = self.addCtl(
+                self.npo,
+                "surface_ctl",
+                t,
+                self.color_ik,
+                "square")
+
+        self.surfRef = self.settings["surfaceReference"]
+        if not self.surfRef:
+            self.sliding_surface = pm.duplicate(self.guide.getObjects(self.guide.root)["sliding_surface"])[0]
+            pm.parent(self.sliding_surface.name(), self.root)
+            self.sliding_surface.visibility.set(False)
+            pm.makeIdentity(self.sliding_surface, apply=True, t=1,  r=1, s=1, n=0, pn=1)
+
+        if self.connect_surface_slider:
+            bt = getTransform(self.root)
+            self.slider_root = addTransform(self.root, self.getName("sliders"), bt)
+            ymt_util.setKeyableAttributesDontLockVisibility(self.slider_root, [])
+
+    # =====================================================
+    # ATTRIBUTES
+    # =====================================================
+    def addAttributes(self):
+        """Create the anim and setupr rig attributes for the component"""
+
+        if not self.settings["ui_host"]:
+            self.uihost = self.surfaceCtl
+
+    # =====================================================
+    # OPERATORS
+    # =====================================================
+    def addOperators(self):
+        """Create operators and set the relations for the component rig
+
+        Apply operators, constraints, expressions to the hierarchy.
+        In order to keep the code clean and easier to debug,
+        we shouldn't create any new object in this method.
+
+        """
+        pass
+
+    def connectRef(self, refArray, cns_obj, upVAttr=None, init_refNames=False):
+        """Connect the cns_obj to a multiple object using parentConstraint.
+
+        Args:
+            refArray (list of dagNode): List of driver objects
+            cns_obj (dagNode): The driven object.
+            upVAttr (bool): Set if the ref Array is for IK or Up vector
+        """
+        if refArray:
+            if upVAttr and not init_refNames:
+                # we only can perform name validation if the init_refnames are
+                # provided in a separated list. This check ensures backwards
+                # copatibility
+                ref_names = refArray.split(",")
+            else:
+                ref_names = self.get_valid_ref_list(refArray.split(","))
+
+            if not ref_names:
+                # return if the not ref_names list
+                return
+            elif len(ref_names) == 1:
+                ref = self.rig.findRelative(ref_names[0])
+                pm.parent(cns_obj, ref)
+            else:
+                ref = []
+                for ref_name in ref_names:
+                    ref.append(self.rig.findRelative(ref_name))
+
+                ref.append(cns_obj)
+                cns_node = pm.parentConstraint(*ref, maintainOffset=True)
+                cns_attr = pm.parentConstraint(
+                    cns_node, query=True, weightAliasList=True)
+                # check if the ref Array is for IK or Up vector
+                try:
+                    if upVAttr:
+                        oAttr = self.upvref_att
+                    else:
+                        oAttr = self.ikref_att
+
+                except AttributeError:
+                    oAttr = None
+
+                if oAttr:
+                    for i, attr in enumerate(cns_attr):
+                        node_name = pm.createNode("condition")
+                        pm.connectAttr(oAttr, node_name + ".firstTerm")
+                        pm.setAttr(node_name + ".secondTerm", i)
+                        pm.setAttr(node_name + ".operation", 0)
+                        pm.setAttr(node_name + ".colorIfTrueR", 1)
+                        pm.setAttr(node_name + ".colorIfFalseR", 0)
+                        pm.connectAttr(node_name + ".outColorR", attr)
+
+    def connect_standard(self):
+        self.parent.addChild(self.root)
+        if self.surfRef:
+            ref = self.rig.findComponent(self.surfRef)
+            self.sliding_surface = ref.sliding_surface
+
+        if self.connect_surface_slider:
+            try:
+                self.connect_slide_ghost()
+
+            except Exception as _:
+                import traceback
+                traceback.print_exc()
+
+        else:
+            try:
+                self.connect_rivet()
+
+            except Exception as _:
+                import traceback
+                traceback.print_exc()
+
+    def connect_slide_ghost(self):
+
+        # create ghost controls
+        self.ghostCtl = ghost.createGhostCtl(self.surfaceCtl, self.slider_root)
+        self.ghostCtl.rename(self.getName("surface_ctl"))
+
+        if self.settings.get("visHost", False):
+            self._visi_off_lock(self.surfaceCtl)
+
+        oParent = self.ghostCtl.getParent()
+        npoName = "_".join(self.ghostCtl.name().split("_")[:-1]) + "_npo"
+        npo = pm.PyNode(pm.createNode("transform", n=npoName, p=oParent, ss=True))
+        npo.addChild(self.ghostCtl)
+
+        self.ghostCtl.attr("isCtl") // self.surfaceCtl.attr("isCtl")
+        self.ghostCtl.attr("translate") // self.surfaceCtl.attr("translate")
+        self.ghostCtl.attr("rotate") // self.surfaceCtl.attr("rotate")
+        self.ghostCtl.attr("scale") // self.surfaceCtl.attr("scale")
+
+        self.surfaceCtl.rename(self.getName("source_ctl"))
+
+        ctl = pm.listConnections(self.ghostCtl, t="transform")[-1]
+        t = ctl.getMatrix(worldSpace=True)
+
+        oParent = self.ghostCtl.getParent()
+        npoName = "_".join(self.ghostCtl.name().split("_")[:-1]) + "_npo"
+        npo = pm.PyNode(pm.createNode("transform", n=npoName, p=oParent, ss=True))
+        npo.setTransformation(self.ghostCtl.getMatrix())
+        ymt_util.setKeyableAttributesDontLockVisibility(npo, [])
+
+        pm.parent(self.ghostCtl, npo)
+
+        slider = primitive.addTransform(
+                self.sliding_surface.getParent(),
+                ctl.name() + "_slideDriven",
+                t)
+
+        down, _, up = ymt_util.findPathAtoB(ctl, self.sliding_surface.getParent())
+        mul_node = pm.createNode("multMatrix")
+        j = k = 0
+        for j, d in enumerate(down):
+            d.attr("matrix") >> mul_node.attr("matrixIn[{}]".format(j))
+        for k, u in enumerate(up):
+            u.attr("inverseMatrix") >> mul_node.attr("matrixIn[{}]".format(k + j + 1))
+
+        dm_node = node.createDecomposeMatrixNode(mul_node.attr("matrixSum"))
+
+        cps_node = pm.createNode("closestPointOnSurface")
+        dm_node.attr("outputTranslate") >> cps_node.attr("inPosition")
+        surfaceShape = self.sliding_surface.getShape()
+        surfaceShape.attr("local") >> cps_node.attr("inputSurface")
+        cps_node.attr("position") >> slider.attr("translate")
+
+        if self.negate:
+            aim = [0, 0, -1]
+        else:
+            aim = [0, 0, 1]
+        pm.normalConstraint(surfaceShape,
+                            slider,
+                            aimVector=aim,
+                            upVector=[0, 1, 0],
+                            worldUpType="objectrotation",
+                            worldUpVector=[0, 1, 0],
+                            worldUpObject=self.root)
+        pm.parent(self.ghostCtl.getParent(), slider)
+        self.jnt_pos.append([self.ghostCtl, "0"])
+
+        if self.surfaceKeyable:
+            self.ghostCtl.attr("isCtl").set(True)
+        else:
+            self.removeFromControllerGroup(self.ghostCtl)
+
+        if self.sourceKeyable:
+            self.surfaceCtl.attr("isCtl").set(True)
+        else:
+            self.removeFromControllerGroup(self.surfaceCtl)
+
+        self.setRelation()  # MUST re-setRelation, swapped ghost and real controls
+
+    def connect_rivet(self):
+        rivets = ymt_util.apply_rivet_constrain_to_selected(self.sliding_surface, self.npo)
+        cmds.parent(rivets[0], self.sliding_surface.getParent().fullPath())
+        cmds.parentConstraint(rivets[0], self.npo.fullPath(), mo=True)
+
+    # =====================================================
+    # CONNECTOR
+    # =====================================================
+    def addConnection(self):
+        self.connections["standard"] = self.connect_standard
+
+    def setRelation(self):
+        """Set the relation beetween object from guide to rig"""
+        self.relatives["root"] = self.root
+        self.relatives["ctl"] = self.surfaceCtl
+
+    # =====================================================
+    # UTILITY
+    # =====================================================
+    def _visi_off_lock(self, node):
+        """Short cuts."""
+        node.visibility.set(False)
+        ymt_util.setKeyableAttributesDontLockVisibility(node, [])
+        cmds.setAttr("{}.visibility".format(node.name()), l=False)
+
+    def removeFromControllerGroup(self, obj):
+        if self.settings["ctlGrp"]:
+            ctlGrp = self.settings["ctlGrp"]
+
+        else:
+            ctlGrp = "controllers"
+
+        if ctlGrp not in self.groups.keys():
+            self.groups[ctlGrp] = []
+
+        try:
+            self.groups[ctlGrp].remove(obj)
+        except Exception:
+            pass
+
+        obj.attr("isCtl").set(False)
+        pm.deleteAttr(obj.attr("isCtl"))
+        ymt_util.setKeyableAttributesDontLockVisibility(obj, [])
+        pm.delete(obj.getShape())
+
+
+if __name__ == "__main__":
+    pass
---
+++ python/ymt_components/ymt_surface_ghost_01/guide.py
@@ -0,0 +1,314 @@
+# pylint: disable=import-error,W0201,C0111,C0112
+from functools import partial
+
+from mgear.shifter.component import guide
+from mgear.core import (
+    pyqt,
+    string,
+    # dag,
+    attribute,
+    transform,
+)
+from mgear.vendor.Qt import QtWidgets, QtCore
+from mgear import shifter
+
+# from mgear.core.primitive import addTransform
+
+
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+from maya.app.general.mayaMixin import MayaQDockWidget
+
+from . import settingsUI as sui
+import pymel.core as pm
+from pymel.core import datatypes
+
+from . import chain_guide_initializer
+
+# guide info
+AUTHOR = "yamahigashi"
+EMAIL = "yamahigashi@gmail.com"
+URL = "github.com/yamahigashi"
+VERSION = [1, 0, 0]
+TYPE = "ymt_surface_ghost_01"
+NAME = "surfaceGhost"
+DESCRIPTION = ""
+
+##########################################################
+# CLASS
+##########################################################
+
+
+class Guide(guide.ComponentGuide):
+    """Component Guide Class"""
+
+    compType = TYPE
+    compName = NAME
+    description = DESCRIPTION
+
+    author = AUTHOR
+    url = URL
+    email = EMAIL
+    version = VERSION
+
+    connectors = ["surface"]
+
+    def postInit(self):
+        """Initialize the position for the guide"""
+
+        self.save_transform = ["root", "sizeRef"]
+
+    def addObjects(self):
+        """Add the Guide Root, blade and locators"""
+
+        self.root = self.addRoot()
+        vTemp = transform.getOffsetPosition(self.root, [0, 0, 1])
+        self.sizeRef = self.addLoc("sizeRef", self.root, vTemp)
+        pm.delete(self.sizeRef.getShapes())
+        attribute.lockAttribute(self.sizeRef)
+
+    def addParameters(self):
+        """Add the configurations settings"""
+
+        self.pUseIndex = self.addParam("useIndex", "bool", False)
+        self.pNeutralPose = self.addParam("neutralpose", "bool", False)
+        self.pOverrideNegate = self.addParam("overrideNegate", "bool", False)
+        self.pSlidingSurface   = self.addParam("isSlidingSurface", "bool", True)
+        self.pSurfaceReference = self.addParam("surfaceReference", "string", "")
+        self.pParentJointIndex = self.addParam("parentJointIndex", "long", -1, None, None)
+        self.pAddJoints = self.addParam("addJoints", "bool", True)
+        self.pSourceKeyable = self.addParam("sourceKeyable", "bool", True)
+        self.pSurfaceKeyable = self.addParam("surfaceKeyable", "bool", True)
+
+    def modalPositions(self):
+        """Launch a modal dialog to set position of the guide."""
+        self.sections_number = None
+        self.dir_axis = None
+        self.spacing = None
+
+        for name in self.save_transform:
+
+            if "#" in name:
+
+                print(name)
+
+                init_window = chain_guide_initializer.exec_window()
+                if init_window:
+                    self.sections_number = init_window.sections_number
+                    self.dir_axis = init_window.dir_axis
+                    self.spacing = init_window.spacing
+
+                # None the action is cancel
+                else:
+                    return False
+
+                if self.sections_number:
+                    if self.dir_axis == 0:  # X
+                        offVec = datatypes.Vector(self.spacing, 0, 0)
+                    elif self.dir_axis == 3:  # -X
+                        offVec = datatypes.Vector(self.spacing * -1, 0, 0)
+                    elif self.dir_axis == 1:  # Y
+                        offVec = datatypes.Vector(0, self.spacing, 0)
+                    elif self.dir_axis == 4:  # -Y
+                        offVec = datatypes.Vector(0, self.spacing * -1, 0)
+                    elif self.dir_axis == 2:  # Z
+                        offVec = datatypes.Vector(0, 0, self.spacing)
+                    elif self.dir_axis == 5:  # -Z
+                        offVec = datatypes.Vector(0, 0, self.spacing * -1)
+
+                    newPosition = datatypes.Vector(0, 0, 0)
+                    for i in range(self.sections_number):
+                        newPosition = offVec + newPosition
+                        localName = string.replaceSharpWithPadding(name, i)
+                        self.tra[localName] = transform.getTransformFromPos(
+                            newPosition)
+        return True
+
+
+##########################################################
+# Setting Page
+##########################################################
+class settingsTab(QtWidgets.QDialog, sui.Ui_Form):
+
+    def __init__(self, parent=None):
+        super(settingsTab, self).__init__(parent)
+        self.setupUi(self)
+
+
+class componentSettings(MayaQWidgetDockableMixin, guide.componentMainSettings):
+
+    def __init__(self, parent=None):
+        self.toolName = TYPE
+        # Delete old instances of the componet settings window.
+        pyqt.deleteInstances(self, MayaQDockWidget)
+
+        super(self.__class__, self).__init__(parent=parent)
+        self.settingsTab = settingsTab()
+
+        self.setup_componentSettingWindow()
+        self.create_componentControls()
+        self.populate_componentControls()
+        self.create_componentLayout()
+        self.create_componentConnections()
+
+    def setup_componentSettingWindow(self):
+        self.mayaMainWindow = pyqt.maya_main_window()
+
+        self.setObjectName(self.toolName)
+        self.setWindowFlags(QtCore.Qt.Window)
+        self.setWindowTitle(TYPE)
+        self.resize(280, 350)
+
+    def create_componentControls(self):
+        pass
+
+    def populate_componentControls(self):
+        """Populate Controls
+
+        Populate the controls values from the custom attributes of the
+        component.
+
+        """
+        # populate tab
+        self.tabs.insertTab(1, self.settingsTab, "Component Settings")
+
+        # populate component settings
+        self.populateCheck(self.settingsTab.overrideNegate_checkBox, "overrideNegate")
+        self.populateCheck(self.settingsTab.addJoints_checkBox, "addJoints")
+        self.populateCheck(self.settingsTab.sourceKeyable_checkBox, "sourceKeyable")
+        self.populateCheck(self.settingsTab.surfaceKeyable_checkBox, "surfaceKeyable")
+
+        try:
+            self.root.attr("isSlidingSurface").get()
+        except Exception:
+            self.root.pSlidingSurface = self.root.addParam("isSlidingSurface", "bool", True)
+        self.populateCheck(self.settingsTab.isSlidingSurface,"isSlidingSurface")
+
+        try:
+            surfaceReference = self.root.attr("surfaceReference").get()
+        except Exception:
+            surfaceReference = ""
+            self.root.pSurfaceReference = self.root.addParam("surfaceReference", "string", "")
+        self.settingsTab.surfaceReference_listWidget.addItem(surfaceReference)
+
+        # populate connections in main settings
+        self.c_box = self.mainSettingsTab.connector_comboBox
+        for cnx in Guide.connectors:
+            self.c_box.addItem(cnx)
+        self.connector_items = [self.c_box.itemText(i) for i in
+                                range(self.c_box.count())]
+
+        currentConnector = self.root.attr("connector").get()
+        if currentConnector not in self.connector_items:
+            self.c_box.addItem(currentConnector)
+            self.connector_items.append(currentConnector)
+            pm.displayWarning(
+                "The current connector: %s, is not a valid connector for this"
+                " component. Build will Fail!!")
+        comboIndex = self.connector_items.index(currentConnector)
+        self.c_box.setCurrentIndex(comboIndex)
+
+    def create_componentLayout(self):
+
+        self.settings_layout = QtWidgets.QVBoxLayout()
+        self.settings_layout.addWidget(self.tabs)
+        self.settings_layout.addWidget(self.close_button)
+
+        self.setLayout(self.settings_layout)
+
+    def create_componentConnections(self):
+
+        # populate component settings
+        self.mainSettingsTab.connector_comboBox.currentIndexChanged.connect(
+            partial(self.updateConnector,
+                    self.mainSettingsTab.connector_comboBox,
+                    self.connector_items))
+
+        self.settingsTab.overrideNegate_checkBox.stateChanged.connect(
+            partial(self.updateCheck,
+                    self.settingsTab.overrideNegate_checkBox,
+                    "overrideNegate"))
+
+        self.settingsTab.addJoints_checkBox.stateChanged.connect(
+            partial(self.updateCheck,
+                    self.settingsTab.addJoints_checkBox,
+                    "addJoints"))
+
+        self.settingsTab.sourceKeyable_checkBox.stateChanged.connect(
+            partial(self.updateCheck,
+                    self.settingsTab.sourceKeyable_checkBox,
+                    "sourceKeyable"))
+
+        self.settingsTab.surfaceKeyable_checkBox.stateChanged.connect(
+            partial(self.updateCheck,
+                    self.settingsTab.surfaceKeyable_checkBox,
+                    "surfaceKeyable"))
+
+        self.settingsTab.isSlidingSurface.stateChanged.connect(
+            partial(self.updateCheck,
+                    self.settingsTab.isSlidingSurface,
+                    "isSlidingSurface"))
+
+        self.settingsTab.surfaceReferenceAdd_pushButton.clicked.connect(
+            partial(
+                self.addItem2listWidget,
+                self.settingsTab.surfaceReference_listWidget,
+                "surfaceReference"
+            )
+        )
+
+        self.settingsTab.surfaceReferenceRemove_pushButton.clicked.connect(
+            partial(
+                self.removeSelectedFromListWidget,
+                self.settingsTab.surfaceReference_listWidget,
+                "surfaceReference"
+            )
+        )
+
+    def updateMasterChain(self, lEdit, targetAttr):
+        oType = pm.nodetypes.Transform
+
+        oSel = pm.selected()
+        compatible = [TYPE]
+        if oSel:
+            if oSel[0] == self.root:
+                pm.displayWarning("Self root can not be Master. Cycle Warning")
+            else:
+                if (isinstance(oSel[0], oType)
+                        and oSel[0].hasAttr("comp_type")
+                        and oSel[0].attr("comp_type").get() in compatible):
+                    # check master chain FK segments
+                    self_len = self._get_chain_segments_length(self.root)
+                    master_len = self._get_chain_segments_length(oSel[0])
+
+                    if master_len >= self_len:
+                        comp_name = oSel[0].name().replace("_root", "")
+                        lEdit.setText(comp_name)
+                        self.root.attr(targetAttr).set(lEdit.text())
+                    else:
+                        pm.displayWarning(
+                            "Invalid Master: {} ".format(oSel[0]) +
+                            "Current chain has: {} sections".format(self_len) +
+                            " But Master chain has" +
+                            " less sections: {}".format(str(master_len)))
+                else:
+                    pm.displayWarning("The selected element is not a "
+                                      "chain root or compatible chain")
+                    pm.displayWarning("Complatible chain componentes"
+                                      " are: {}".format(str(compatible)))
+        else:
+            pm.displayWarning("Nothing selected.")
+            if lEdit.text():
+                lEdit.clear()
+                self.root.attr(targetAttr).set("")
+                pm.displayWarning("The previous Master Chain have been "
+                                  "cleared")
+
+    def _get_chain_segments_length(self, chain_root):
+        module = shifter.importComponentGuide(chain_root.comp_type.get())
+        componentGuide = getattr(module, "Guide")
+        comp_guide = componentGuide()
+        comp_guide.setFromHierarchy(chain_root)
+        return len(comp_guide.pos)
+
+    def dockCloseEventTriggered(self):
+        pyqt.deleteInstances(self, MayaQDockWidget)
